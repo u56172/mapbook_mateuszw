511d95a42fee06fe48544962366b79908dd82973
Mateusz <u56172@student.wat.edu.pl>
2025-10-29 09:41:25 +0100

    w&p: added a list with users, locations and posts

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ zmienna_2:str = 'Michasia'
 zmienna_3:str = 'Zdzisia'
 zmienna_4:str = 'Mateusz'
 
-#przerób istniejący kod w taki sposób aby zdefinowani użytkownicy byli witani za pomocą pęli
-uzytkownicy =[zmienna_1, zmienna_2, zmienna_3, zmienna_4]
-for zmienna in uzytkownicy:
-    print(f'Hello {zmienna}')
+#przygotuj liste slownikow ktora bedzie przechowywac informacje o uzytkownikach na temat: imie(alias name)
+#miejscowosc (location), liczba_postow(posts)
+users:list = [
+    {'name':zmienna_1, 'location': 'Washington', 'posts': 67},
+    {'name':zmienna_2, 'location': 'Paris', 'posts': 22},
+    {'name':zmienna_3, 'location': 'Michigan', 'posts': 55},
+    {'name':zmienna_4, 'location': 'Warsaw', 'posts': 3}
+    ]
+
+for user in users:
+    print(f'Your user {user['name']}, from {user['location']}, has {user['posts']} posts.')
