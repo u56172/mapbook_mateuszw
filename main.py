2610651ac40ee490eb0972b8d3f7d3cf360ecc6e
Mateusz <u56172@student.wat.edu.pl>
2025-10-29 09:10:21 +0100

    w&p: added for loop with users

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello World')
+zmienna_1:str = 'Kasia'
+zmienna_2:str = 'Michasia'
+zmienna_3:str = 'Zdzisia'
+zmienna_4:str = 'Mateusz'
+
+#przerób istniejący kod w taki sposób aby zdefinowani użytkownicy byli witani za pomocą pęli
+uzytkownicy =[zmienna_1, zmienna_2, zmienna_3, zmienna_4]
+for zmienna in uzytkownicy:
+    print(f'Hello {zmienna}')
