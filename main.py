e97c9defabd0adf52a52df8415c5b85e0f2874c6
Mateusz <u56172@student.wat.edu.pl>
2025-10-29 11:01:16 +0100

    w&p: added function for adding user info

--- main.py
+++ main.py
@@ -15,6 +15,16 @@ def user_info(users_data: list) -> None:
     for user in users_data:
         print(f'Your user {user['name']}, from {user['location']}, has {user['posts']} posts.')
 
+def add_user(users_data: list) -> None:
+    print('Wybrano funkcje dodawania znajomego')
+    name: str = input('Enter your name: ')
+    location: str = input('Enter your location: ')
+    posts: int = int(input('Enter number of posts: '))
+    users_data.append({'name': name, 'location': location, 'posts': posts})
+    print('User added!')
+
+
+
 if __name__ == '__main__':
     while True:
         tmp_choice:int= int(input('Wybierz opcje menu: '))
@@ -23,7 +33,7 @@ if __name__ == '__main__':
         if tmp_choice == 1:
             user_info(users)
         if tmp_choice == 2:
-            print('Wybrano funkcje dodawania znajomego')
+            add_user(users)
         if tmp_choice == 3:
             print('Wybrano funkcje usuwania znajomych')
         if tmp_choice == 4:
