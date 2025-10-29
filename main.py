909c5df57ad80f0117cb19d04caf2c916ff88b8d
Mateusz <u56172@student.wat.edu.pl>
2025-10-29 10:36:48 +0100

    w&p: added function to display user info

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-zmienna_1:str = 'Kasia'
-zmienna_2:str = 'Michasia'
-zmienna_3:str = 'Zdzisia'
-zmienna_4:str = 'Mateusz'
+zmienna_1: str = 'Kasia'
+zmienna_2: str = 'Michasia'
+zmienna_3: str = 'Zdzisia'
+zmienna_4: str = 'Mateusz'
 
-#przygotuj liste slownikow ktora bedzie przechowywac informacje o uzytkownikach na temat: imie(alias name)
-#miejscowosc (location), liczba_postow(posts)
-users:list = [
-    {'name':zmienna_1, 'location': 'Washington', 'posts': 67},
-    {'name':zmienna_2, 'location': 'Paris', 'posts': 22},
-    {'name':zmienna_3, 'location': 'Michigan', 'posts': 55},
-    {'name':zmienna_4, 'location': 'Warsaw', 'posts': 3}
-    ]
+users: list = [
+    {'name': zmienna_1, 'location': 'Washington', 'posts': 67},
+    {'name': zmienna_2, 'location': 'Paris', 'posts': 22},
+    {'name': zmienna_3, 'location': 'Michigan', 'posts': 55},
+    {'name': zmienna_4, 'location': 'Warsaw', 'posts': 3}
+]
 
-for user in users:
-    print(f'Your user {user['name']}, from {user['location']}, has {user['posts']} posts.')
+def user_info(users_data: list) -> None:
+    print('Wybrano funkcje wyświetlania aktywności znajomych')
+    for user in users_data:
+        print(f'Your user {user['name']}, from {user['location']}, has {user['posts']} posts.')
+
+if __name__ == '__main__':
+    while True:
+        tmp_choice:int= int(input('Wybierz opcje menu: '))
+        if tmp_choice == 0:
+            break
+        if tmp_choice == 1:
+            user_info(users)
+        if tmp_choice == 2:
+            print('Wybrano funkcje dodawania znajomego')
+        if tmp_choice == 3:
+            print('Wybrano funkcje usuwania znajomych')
+        if tmp_choice == 4:
+            print('Wybrano funkcje aktualizacji znajomych')
